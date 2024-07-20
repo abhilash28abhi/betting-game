285816d7910eeb882b1b25f8432bd47f8729cd69
Abhilash <abhilash@ge.com>
2024-07-20 16:39:29 +0530

    added dependency for open api

---
+++ src/main/java/com/betting/api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.betting.api.exceptions;
+
+import com.betting.api.errors.HttpErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex,
+                                                                        WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        HttpErrorResponse errorResponse = new HttpErrorResponse(
+                LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Request",
+                errors.toString(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
